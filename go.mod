77a5586906efb176471fb24dbe5b293dd94a02f2
Todd Resudek <toddresudek@gmail.com>
2020-06-15 10:50:09 -0600

    tHis Is tHE wHoLE tHInG

---
+++ go.mod
@@ -0,0 +1,5 @@
+module spongebob
+
+go 1.13
+
+require github.com/gookit/color v1.2.5
---
+++ spongebob.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gookit/color"
+)
+
+func randomCase(l string) string {
+	rand := rand.Intn(42)
+	flip := rand % 2
+	if flip == 0 {
+		return strings.ToUpper(l)
+	}
+	return strings.ToLower(l)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		color.Error.Println("Error: a string is required.")
+		os.Exit(1)
+	}
+	str := os.Args[1]
+	letters := strings.Split(str, "")
+	rand.Seed(time.Now().UnixNano())
+	output := ""
+	for _, l := range letters {
+		output = output + randomCase(l)
+	}
+	fmt.Println(output)
+}
